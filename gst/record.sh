67290756d9211784de119441f287be59f73ee0ff
airpark69 <rudah69@gmail.com>
2024-07-20 01:50:53 +0900

    gst record.sh 업데이트

--- gst/record.sh
+++ gst/record.sh
@@ -27,39 +27,42 @@ trap cleanup INT TERM
 
 VIDEO_INPUT=$(ls /dev/video* | grep -o "[0-9]")
 
+start_time=0900
+end_time=1730
+
 while true; do
-    # 현재 시간을 가져오기 (HHMM 형식, (KST, UTC+9) 기준)
     current_time=$(TZ='Asia/Seoul' date +"%H%M")
 
-    # 아침 8:30 (0830)과 저녁 5:30 (1730) 시간 정의
-    start_time=0900
-    end_time=1730
-
-    # 현재 시간이 실행 시간 내에 있는지 확인
     if [ "$current_time" -ge "$start_time" ] && [ "$current_time" -le "$end_time" ]; then
-        # 명령어 실행
-        video-viewer csi://"$VIDEO_INPUT" "$SERVER_RTP_SET" --input-width=1280 --input-height=720 --input-rate=30/1 --input-codec=mjpeg --output-codec=h264 --output-encoder=cpu --bitrate=2000000 --headless > /var/log/video-viewer.log 2>&1 &
+        if ! pgrep -x "video-viewer" > /dev/null; then
+            echo "녹화 시작 시간입니다. video-viewer를 실행합니다."
+            video-viewer csi://"$VIDEO_INPUT" "$SERVER_RTP_SET" --input-width=1280 --input-height=720 --input-rate=30/1 --input-codec=mjpeg --output-codec=h264 --output-encoder=cpu --bitrate=2000000 --headless > /var/log/video-viewer.log 2>&1 &
+        fi
     else
-        echo "현재 시간은 명령어 실행 시간이 아닙니다."
-        sleep 60
-        continue
+        if pgrep -x "video-viewer" > /dev/null; then
+            echo "녹화 시간이 아닙니다. video-viewer를 종료합니다."
+            pkill -2 -x "video-viewer"
+        fi
     fi
 
-    # 로그를 지속적으로 확인
-    tail -f /var/log/video-viewer.log | while read line; do
-        echo "$line"
-        if [[ "$line" == *"gstCamera::Capture() -- a timeout occurred waiting for the next image buffer"* ]]; then
-            echo "Timeout detected. Restarting gst-launch-1.0..."
-            pkill -2 -P $$
-            break
-        elif [[ "$line" == *"video-viewer:  failed to create output stream"* ]]; then
-            echo "다른 조건문 matched. Restarting gst-launch-1.0..."
-            pkill -2 -P $$
-            break
-        fi
-    done
+    # 로그 파일 모니터링을 백그라운드에서 실행
+    if pgrep -x "video-viewer" > /dev/null; then
+        (tail -f /var/log/video-viewer.log | while read line; do
+            echo "$line"
+            if [[ "$line" == *"gstCamera::Capture() -- a timeout occurred waiting for the next image buffer"* ]]; then
+                echo "Timeout detected. Restarting video-viewer..."
+                pkill -2 -x "video-viewer"
+                service nvargus-daemon restart
+                break
+            elif [[ "$line" == *"video-viewer:  failed to create output stream"* ]]; then
+                echo "다른 조건문 matched. Restarting video-viewer..."
+                pkill -2 -x "video-viewer"
+                service nvargus-daemon restart
+                break
+            fi
+        done) &
+    fi
+
     # 잠시 대기 (너무 빠른 재시작 방지)
-    sleep 5
-    service nvargus-daemon restart
-    sleep 5
+    sleep 60
 done
